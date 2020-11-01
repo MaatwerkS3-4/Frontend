e3613c5eab5401a943368634626e8773065d9aad
Njols <niels@tonsoftime.com>
2020-11-01 13:10:31 +0100

    small improvements to the createpost page

--- opinion-app/src/createPost.jsx
+++ opinion-app/src/createPost.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./createPost.css";
-import {postPost} from "./apiRequests.js";
-import {getPostBySubString} from "./apiRequests.js";
+import { postPost } from "./apiRequests.js";
+import { getPostBySubString } from "./apiRequests.js";
 class createPost extends Component {
   constructor(props) {
     super(props);
@@ -10,27 +10,27 @@ class createPost extends Component {
       subjects: [
         {
           id: 1,
-          subject : "Zwarte Piet"
+          subject: "Zwarte Piet",
         },
         {
           id: 2,
-          subject: "Klimaatverandering"},
-        {id : 3, subject : "De Amerikaanse Verkiezingen"},
-        {id: 4, subject : "Nederlands Migratiebeleid"},
-        {id : 5, subject : "De EU"}
+          subject: "Klimaatverandering",
+        },
+        { id: 3, subject: "De Amerikaanse Verkiezingen" },
+        { id: 4, subject: "Nederlands Migratiebeleid" },
+        { id: 5, subject: "De EU" },
       ],
       modal: false,
-      showDataList : false,
-      show : false
+      showDataList: false,
+      show: false,
     };
   }
   handleInputOnChange = (event) => {
     if (document.getElementById("subject-input").value.length >= 4) {
-      this.setState({showDataList : true});
+      this.setState({ showDataList: true });
       this.updateSuggestList(document.getElementById("subject-input").value);
-    }
-    else {
-      this.setState({showDataList : false});
+    } else {
+      this.setState({ showDataList: false });
     }
     if (
       this.state.subjects.some(
@@ -47,8 +47,7 @@ class createPost extends Component {
   handleButtonPress = (event) => {
     if (this.state.contains) {
       this.toggleModal();
-    }
-    else {
+    } else {
       this.makePost();
     }
   };
@@ -60,39 +59,62 @@ class createPost extends Component {
     }
   };
   makePost = (event) => {
-    let value = document.getElementById("subject-input").value.toString()
+    let value = document.getElementById("subject-input").value.toString();
     let post = {
-      subject : "Tomatoes",
-      user : {
-        id : 1,
-        username : "Potatoman",
-        password : "Yes"
-      }
-    }
+      subject: value,
+      user: {
+        id: 1,
+        username: "Potatoman",
+        password: "Yes",
+      },
+    };
     postPost(post);
+    this.setState({ show: false });
   };
   updateSuggestList = (substring) => {
     getPostBySubString(substring).then((response) => {
       if (response.data !== null) {
-        this.setState({subjects : response.data})
+        this.setState({ subjects: response.data });
       }
-    })
+    });
   };
   closeButtonClick = (event) => {
-    this.setState({show : false})
-  }
+    this.setState({ show: false });
+  };
   componentWillReceiveProps(nextProps) {
     if (typeof nextProps.show === "boolean") {
-      this.setState({show : !this.state.show})
+      this.setState({ show: !this.state.show });
     }
   }
   render() {
     return (
       <div id="container">
-        <div className={this.state.show ? "show card" : "no-show"} id="create-post-card">
-          <div className="card-header"><span className="title">create a post</span><button type="button" className="btn btn-danger close-button" onClick={this.closeButtonClick}><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x" fill="white" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-</svg></button>     </div>
+        <div
+          className={this.state.show ? "show card" : "no-show"}
+          id="create-post-card"
+        >
+          <div className="card-header">
+            <span className="title">create a post</span>
+            <button
+              type="button"
+              className="btn btn-danger close-button"
+              onClick={this.closeButtonClick}
+            >
+              <svg
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                class="bi bi-x"
+                fill="white"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
+                />
+              </svg>
+            </button>{" "}
+          </div>
           <div className="card-body">
             What's your opinion about{" "}
             <input
@@ -103,13 +125,13 @@ class createPost extends Component {
               id="subject-input"
               onInput={this.handleInputOnChange}
             ></input>
-            { this.state.showDataList &&
-            <datalist id="dropdown">
-              {this.state.subjects.map((subject) => (
-                <option>{subject.subject}</option>
-              ))}
-            </datalist>
-  }{" "}
+            {this.state.showDataList && (
+              <datalist id="dropdown">
+                {this.state.subjects.map((subject) => (
+                  <option>{subject.subject}</option>
+                ))}
+              </datalist>
+            )}{" "}
             <br />
             <button
               type="button"
@@ -140,12 +162,18 @@ class createPost extends Component {
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-              <div className="modal-body">A post about this subject might already exist</div>
+              <div className="modal-body">
+                A post about this subject might already exist
+              </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-primary">
                   Go to duplicate
                 </button>
-                <button type="button" className="btn btn-secondary" onClick={this.makePost}>
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={this.makePost}
+                >
                   Create anyways
                 </button>
               </div>
