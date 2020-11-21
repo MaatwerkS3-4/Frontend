8902380298442d2076ff81a1ef0bd716c6b230ee
Ruben <24549041+RDieleman@users.noreply.github.com>
2020-11-21 16:11:24 +0100

    added searchbox functionality to homepage

--- opinion-app/src/pages/home/home.component.jsx
+++ opinion-app/src/pages/home/home.component.jsx
@@ -1,21 +1,36 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import CreatePost from "../../components/create-discussion/create-discussion.component.jsx";
 import DiscussionListComponent from "../discussions-list/discussion-list.component.jsx";
 import "./home.styles.css";
+import {SearchBox} from "../../components/search-box/search-box.component";
 
 class HomeComponent extends Component {
-  state = {
-    showCreatePost: false,
-  };
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            searchField: ''
+        };
+    }
 
-  render() {
-    return (
-      <div id="container">
-        <h1>homepage</h1>
-      </div>
-    );
-  }
+    handleSearchPress = (e) => {
+        console.log(`handle search press button pressed: ${this.state.searchField}`);
+    }
+
+    handleInputChange = (e) => {
+        this.setState({searchField: e.target.value})
+    }
+
+    render() {
+        return (
+            <div id="container">
+                <h1>Wat moet ik vinden van...</h1>
+                <SearchBox placeholder='Zoeken...'
+                           handleInputChange={this.handleInputChange}
+                           handleSearchPress={this.handleSearchPress}/>
+            </div>
+        );
+    }
 }
 
 export default HomeComponent;
