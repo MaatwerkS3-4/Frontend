eaefc9b8658f710fea9aaff6474a52ba4bc3d136
Ruben <24549041+RDieleman@users.noreply.github.com>
2020-11-21 15:16:42 +0100

    created basic discussion detail component

--- opinion-app/src/pages/discussiondetail/discussiondetail.component.jsx
+++ opinion-app/src/pages/discussiondetail/discussiondetail.component.jsx
@@ -1,4 +1,24 @@
-import React from "react";
+import React, {Component} from "react";
 import "./discussiondetail.styles.css";
 
+class DiscussiondetailComponent extends Component{
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Discussion detail page</h1>
+            </div>
+        )
+    }
+}
+
+export default DiscussiondetailComponent;
 
