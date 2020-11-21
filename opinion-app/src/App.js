824d08dc4ed2dcdd3b1138bce7d2a177e298d096
Ruben <24549041+RDieleman@users.noreply.github.com>
2020-11-21 15:13:01 +0100

    renamed discussion detail page

--- opinion-app/src/App.js
+++ opinion-app/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import Discussions from "./pages/discussionslist/discussionlist.component.jsx";
-import DiscussionDetails from "./pages/discussiondetails/discussiondetails.component.jsx";
+import DiscussionDetail from "./pages/discussiondetail/discussiondetail.component.jsx";
 
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
     <Switch> 
       <Route exact path='/' component={HomePage} />
       <Route exact path='/discussions' component={Discussions} />
-      <Route path='/discussions/:discussionId' component={DiscussionDetails} />
+      <Route path='/discussions/:discussionId' component={DiscussionDetail} />
     </Switch> 
     </div>
     </Router>
