11921441614f3c697543a65012c10fcbce1a7958
Ruben <24549041+RDieleman@users.noreply.github.com>
2020-11-21 15:10:02 +0100

    Added routing to discussion- list and detail pages, and created detail page component

--- opinion-app/src/App.js
+++ opinion-app/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'; 
 import "./App.css";
-import HomepageComponent from "./pages/homepage/homepage.component.jsx";
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import Discussions from "./pages/discussionslist/discussionlist.component.jsx";
+import DiscussionDetails from "./pages/discussiondetails/discussiondetails.component.jsx";
+
 
 function App() {
   return (
     <Router>
     <div className="App">
     <Switch> 
-      <Route exact path='/' component={HomepageComponent} />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/discussions' component={Discussions} />
+      <Route path='/discussions/:discussionId' component={DiscussionDetails} />
     </Switch> 
     </div>
     </Router>
