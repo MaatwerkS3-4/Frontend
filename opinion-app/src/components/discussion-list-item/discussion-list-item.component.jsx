4fc5af934cab5b4c3b947754c061912ec164f280
Njols <niels@tonsoftime.com>
2020-11-27 13:54:01 +0100

    improved styling for the discussion list

--- opinion-app/src/components/discussion-list-item/discussion-list-item.component.jsx
+++ opinion-app/src/components/discussion-list-item/discussion-list-item.component.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./discussion-list-item.styles.css";
 
-export const DiscussionListItem = ({discussion}) => {
-    return (
-        <div className="post-list-item" >
-            <span className="subject">{discussion.subject}</span>{" "}
-            <span className="username">{discussion.user.username}</span>
-        </div>
-    )
-}
+export const DiscussionListItem = ({ discussion }) => {
+  return (
+    <div className="post-list-item">
+      <div className="main-info">
+        <span className="subject">{discussion.subject}</span>{" "}
+      </div>
+      <div className="extra-info">
+        <span className="extra-info-item">x Comments</span>
+        <div className="seperator"></div>
+        <span className="extra-info-item">x Deelnemers</span>
+        <div className="seperator"></div>
+        <span className="extra-info-item">x seconden geleden</span>
+      </div>
+    </div>
+  );
+};
--- opinion-app/src/components/discussion-list/discussion-list.component.jsx
+++ opinion-app/src/components/discussion-list/discussion-list.component.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import "./discussion-list.styles.css";
 
-import {DiscussionListItem} from "../discussion-list-item/discussion-list-item.component";
+import { DiscussionListItem } from "../discussion-list-item/discussion-list-item.component";
 
-export const DiscussionList = ({discussions, handleSelectDiscussion, handleRedirect}) => {
-    return (
-        <ul id="post-list">
-            {discussions.map((d, index) => (
-            <li key={index} onClick={() => {
-                handleSelectDiscussion(d);
-                handleRedirect(d.id);
-            }}>
-                <DiscussionListItem  discussion={d}/>
-            </li>
-            ))}
-        </ul>
-    )
-}
+export const DiscussionList = ({
+  discussions,
+  handleSelectDiscussion,
+  handleRedirect,
+}) => {
+  return (
+    <ul id="post-list">
+      {discussions.map((d, index) => (
+        <li
+          key={index}
+          onClick={() => {
+            handleSelectDiscussion(d);
+            handleRedirect(d.id);
+          }}
+        >
+          <DiscussionListItem discussion={d} />
+        </li>
+      ))}
+    </ul>
+  );
+};
