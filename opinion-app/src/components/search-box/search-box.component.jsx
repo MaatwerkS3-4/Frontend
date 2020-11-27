8b0875372697cf8f43854fc5a5cd7fac41bb5344
Njols <niels@tonsoftime.com>
2020-11-27 14:48:22 +0100

    improved styling for the search page

--- opinion-app/src/components/search-box/search-box.component.jsx
+++ opinion-app/src/components/search-box/search-box.component.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import "./search-box.styles.css";
+import { FaSearch } from "react-icons/fa";
 
-export const SearchBox = ({placeholder, handleInputChange, handleSearchPress}) =>{
-    return (
-        <div>
-            <input type='search'
-                   className='search'
-                   onChange={handleInputChange}
-                   placeholder={placeholder}
-            />
-            <button onClick={handleSearchPress}>Search</button>
-        </div>
-    );
+export const SearchBox = ({
+  placeholder,
+  handleInputChange,
+  handleSearchPress,
+}) => {
+  return (
+    <div>
+      <input
+        type="search"
+        className="search"
+        onChange={handleInputChange}
+        placeholder={placeholder}
+      />
+      <button onClick={handleSearchPress} className="search-button">
+        <FaSearch color="white" size={20} />
+      </button>
+    </div>
+  );
 };
--- opinion-app/src/pages/home/home.page.jsx
+++ opinion-app/src/pages/home/home.page.jsx
@@ -1,35 +1,39 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./home.styles.css";
-import {SearchBox} from "../../components/search-box/search-box.component";
+import { SearchBox } from "../../components/search-box/search-box.component";
 
 class HomePage extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            searchField: ''
-        };
-    }
+    this.state = {
+      searchField: "",
+    };
+  }
 
-    handleSearchPress = (e) => {
-        console.log(`handle search press button pressed: ${this.state.searchField}`);
-        this.props.history.push(`/discussions/${this.state.searchField}`);
-    }
+  handleSearchPress = (e) => {
+    console.log(
+      `handle search press button pressed: ${this.state.searchField}`
+    );
+    this.props.history.push(`/discussions/${this.state.searchField}`);
+  };
 
-    handleInputChange = (e) => {
-        this.setState({searchField: e.target.value})
-    }
+  handleInputChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
 
-    render() {
-        return (
-            <div id="container">
-                <h1>Wat moet ik vinden van...</h1>
-                <SearchBox placeholder='Zoeken...'
-                           handleInputChange={this.handleInputChange}
-                           handleSearchPress={this.handleSearchPress}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div id="search-container">
+        <span className="title search-title">Wat moet ik vinden van...</span>
+        <SearchBox
+          placeholder="Zoeken..."
+          handleInputChange={this.handleInputChange}
+          handleSearchPress={this.handleSearchPress}
+        />
+      </div>
+    );
+  }
 }
 
 export default HomePage;
