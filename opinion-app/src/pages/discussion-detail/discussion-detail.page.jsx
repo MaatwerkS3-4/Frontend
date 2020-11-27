d8367b87caff1b63bcb0cfeb061a907e9e58f313
Wesley <wesley.verhagen@student.fontys.nl>
2020-11-27 16:04:25 +0100

    merge conflict fix

--- opinion-app/src/pages/discussion-detail/discussion-detail.page.jsx
+++ opinion-app/src/pages/discussion-detail/discussion-detail.page.jsx
@@ -7,15 +7,8 @@ import {ButtonAttention} from "../../components/button-attention/button-attentio
 import CommentCreate from "../../components/comment-create/comment-create.component";
 
 class DiscussionDetailPage extends Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.state = {
-      id: props.match.params.id,
-      comments: [],
-      showCreateComment: false,
-    };
-  }
+    constructor(props, context) {
+        super(props, context);
 
         this.state = {
             id: props.match.params.id,
@@ -24,105 +17,68 @@ class DiscussionDetailPage extends Component {
             activeTime: 10
         };
     }
-  componentDidCatch(error, errorInfo) {
-    console.log("error caught");
-    this.props.history.push("/");
-  }
 
-  componentDidMount() {
-    this.props.handleToggleLoading(true);
-    getPostComments(this.state.id)
-      .then((response) => {
-        if (response !== undefined && response.data != null) {
-          this.setState({ comments: response.data });
+    componentDidCatch(error, errorInfo) {
+        console.log("error caught");
+        this.props.history.push("/");
+    }
+
+    componentDidMount() {
+        this.props.handleToggleLoading(true);
+        getPostComments(this.state.id)
+            .then((response) => {
+                if (response !== undefined && response.data != null) {
+                    this.setState({comments: response.data});
+                }
+            })
+            .finally(this.props.handleToggleLoading(false));
+    }
+
+    handlePostComment = (comment) => {
+        if (comment !== undefined) {
+            this.props.handleToggleLoading(true);
+            //Creating new comment object
+            console.log("Creating new comment...");
+            console.log(comment);
+
+            //Send object to backend
+            console.log("Sending comment to backend...");
+            postComment(comment)
+                .then((result) => {
+                    this.addComment(result.data);
+                })
+                .finally(() => this.props.handleToggleLoading(false));
         }
-      })
-      .finally(this.props.handleToggleLoading(false));
-  }
 
-  handlePostComment = (comment) => {
-    if (comment !== undefined) {
-      this.props.handleToggleLoading(true);
-      //Creating new comment object
-      console.log("Creating new comment...");
-      console.log(comment);
+        //Close input popup
+        console.log("Closing popup...");
+        this.handleToggleCreateComment();
+    };
 
-      //Send object to backend
-      console.log("Sending comment to backend...");
-      postComment(comment)
-        .then((result) => {
-          this.addComment(result.data);
-        })
-        .finally(() => this.props.handleToggleLoading(false));
-    }
+    addComment = (comment) => {
+        const newComments = [...this.state.comments];
+        newComments.push(comment);
+        this.setState({comment: newComments});
+    };
 
-    //Close input popup
-    console.log("Closing popup...");
-    this.handleToggleCreateComment();
-  };
-
-  addComment = (comment) => {
-    const newComments = [...this.state.comments];
-    newComments.push(comment);
-    this.setState({ comment: newComments });
-  };
-
-  handleToggleCreateComment = () => {
-    this.setState({ showCreateComment: !this.state.showCreateComment });
-  };
-
-  render() {
-    if (this.props.selectedDiscussion === undefined) {
-      this.props.history.push("/");
-    } else {
-      const discussion = this.props.selectedDiscussion;
-      console.log(discussion);
-      return (
-        <div>
-          <h1>Discussion detail page for id {discussion.id}</h1>
-          <DiscussionInfo
-            subject={discussion.subject}
-            username={discussion.username}
-          />
-          <hr />
-          <ButtonAttention
-            content="Reageer"
-            handleClick={this.handlePostComment}
-          />
-          <hr />
-          <h2>Opmerkingen</h2>
-          <CommentList comments={this.state.comments} />
-          {this.state.showCreateComment ? (
-            <CommentCreate
-              handlePostComment={this.handlePostComment}
-              user={this.props.user}
-              parentDiscussion={discussion}
-            />
-          ) : (
-            ""
-          )}
-        </div>
-      );
-    }
 
     handleToggleCreateComment = () => {
         this.setState({showCreateComment: !this.state.showCreateComment});
-    }
+    };
 
     handleBackToOverviewClick = () => {
         this.props.history.push(`/discussions`);
-    }
+    };
 
     render() {
-        if(this.props.selectedDiscussion === undefined){
+        if (this.props.selectedDiscussion === undefined) {
             this.props.history.push("/");
-        }
-        else{
+        } else {
             const discussion = this.props.selectedDiscussion;
             console.log(discussion);
             return (
                 <div className={styles.text}>
-                    <h1 >Discussion detail page for id {discussion.id}</h1>
+                    <h1>Discussion detail page for id {discussion.id}</h1>
                     <DiscussionInfo subject={discussion.subject}
                                     username={discussion.user.username}/>
                     <hr/>
@@ -130,10 +86,12 @@ class DiscussionDetailPage extends Component {
                     <span className={styles.spanPadding}>{this.state.comments.length} deelnemers |</span>
                     <span className={styles.spanPadding}>{this.state.activeTime} seconden geleden</span>
 
-                    <ButtonAttention content="Reageer" handleClick={this.handlePostComment} />
-                    <button className={styles.overviewButton} onClick={this.handleBackToOverviewClick}>terug naar overzicht</button>
+                    <ButtonAttention content="Reageer" handleClick={this.handlePostComment}/>
+                    <button className={styles.overviewButton} onClick={this.handleBackToOverviewClick}>terug naar
+                        overzicht
+                    </button>
                     <h2 className={styles.commentsHeader}>Opmerkingen</h2>
-                    <CommentList comments={this.state.comments} />
+                    <CommentList comments={this.state.comments}/>
                     {this.state.showCreateComment ? <CommentCreate
                         handlePostComment={this.handlePostComment}
                         user={this.props.user}
@@ -144,5 +102,4 @@ class DiscussionDetailPage extends Component {
         }
     }
 }
-
 export default DiscussionDetailPage;
