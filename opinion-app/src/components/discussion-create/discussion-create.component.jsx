b420d6eac4874dd9e9f733f2bf619e0ca2e188e4
Ruben <24549041+RDieleman@users.noreply.github.com>
2020-12-07 02:01:52 +0100

    finished create discussion popup layout

--- opinion-app/src/components/discussion-create/discussion-create.component.jsx
+++ opinion-app/src/components/discussion-create/discussion-create.component.jsx
@@ -3,41 +3,58 @@ import {TextBox} from "../text-box/text-box.component";
 import "./discussion-create.styles.css";
 import {ButtonAttention} from "../button/button-attention/button-attention.component";
 import {ButtonRegular} from "../button/button-regular/button-regular.component";
+import {TextArea} from "../text-area/text-area.component";
 
-class DiscussionCreateComponent extends Component{
+class DiscussionCreateComponent extends Component {
 
     constructor(props, context) {
         super(props, context);
         this.state = {
             subject: '',
+            description: '',
         }
     };
 
-    handleSubjectInputChange = (e) =>{
+    handleSubjectInputChange = (e) => {
         this.setState({subject: e.target.value});
-    }
+    };
 
-    handleCreateClick = () =>{
+    handleDescriptionInputChange = (e) => {
+        this.setState({description: e.target.value});
+    };
+
+    handleCreateClick = () => {
         const discussion = {
             subject: this.state.subject,
             user: this.props.user
         };
-        this.props.handleCreateDiscussion (discussion);
+        this.props.handleCreateDiscussion(discussion);
     }
 
-    handleCloseClick = () =>{
+    handleCloseClick = () => {
         this.props.handleCreateDiscussion();
     }
 
     render() {
-        return(
-            <div className="container">
-                <TextBox tag="Onderwerp"
-                         placeholder=""
-                         handleInputChange={this.handleSubjectInputChange}
-                />
-                <ButtonAttention handleOnClick={this.handleCreateClick} text="Aanmaken" />
-                <ButtonRegular handleOnClick={this.handleCloseClick} text="Sluiten" />
+        return (
+            <div className="create-container">
+                <div className="create-discussion-container">
+                    <div className="discussion-title">Wat moet ik vinden van...</div>
+                    <TextBox tag="Onderwerp"
+                             placeholder=""
+                             handleInputChange={this.handleSubjectInputChange}
+                    />
+                    <TextArea tag="Toelichting"
+                              placeholder=""
+                              handleInputChange={this.handleDescriptionInputChange}
+                    />
+                    <div className="create-discussion-options">
+                        <ButtonRegular handleOnClick={this.handleCloseClick} text="Sluiten"/>
+                        <ButtonAttention handleOnClick={this.handleCreateClick} text="Aanmaken"/>
+                    </div>
+
+                </div>
+                <div className="create-discussion-overlay" onClick={() => this.handleCloseClick()}/>
             </div>
         )
     }
