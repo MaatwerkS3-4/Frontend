36346ee65bda68786ab3af7718a6071253929d58
Ruben <24549041+RDieleman@users.noreply.github.com>
2020-12-08 23:31:01 +0100

    added comments to api requests

--- opinion-app/src/services/api.service.js
+++ opinion-app/src/services/api.service.js
@@ -1,30 +1,36 @@
 import {AXIOS} from "../http-common"
 
 export const handleGetDiscussionById = (id) =>{
+    console.log(`Retrieving discussion with id ${id}...`)
     return AXIOS.get(`/discussion/${id}`)
         .then(response =>{return response.data});
 };
 
 export const handlePostNewDiscussion = (createDiscussionDTO) =>{
+    console.log(`Posting new discussion:`, createDiscussionDTO);
     return AXIOS.post(`/discussion`, createDiscussionDTO)
         .then(res =>{ return res.data});
 };
 export const handlePostNewComment = (discussionId, createCommentDTO) =>{
+    console.log(`Posting new comment on discussion with id ${discussionId}:`, createCommentDTO);
     return AXIOS.post(`/comment/${discussionId}`, createCommentDTO)
         .then(res => {return res.data})
 };
 
 export const handleGetAllDiscussionInfos = () => {
-  return AXIOS.get(`/discussion_info`)
-      .then(res => {return res.data})
+    console.log(`Retrieving all discussion infos...`);
+    return AXIOS.get(`/discussion_info`)
+        .then(res => {return res.data});
 };
 
 export const handleGetDiscussionInfoById = (discussionId) =>{
+    console.log(`Retrieving discussion info with id ${discussionId}...`);
     return AXIOS.get(`/discussion_info/id/${discussionId}`)
         .then(res => {return res.data})
 };
 
 export const handleGetUserById = (id) =>{
+    console.log(`Retrieving user with id ${id}...`)
     return AXIOS.get(`/users/${id}`)
         .then(res => {return res.data})
 };
