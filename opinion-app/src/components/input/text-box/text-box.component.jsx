5479a54a0535e9304c836ba11b0044f0a1235966
Njols <niels@tonsoftime.com>
2020-12-22 11:50:14 +0100

    added password functionality to the textboxes

--- opinion-app/src/components/input/text-box/text-box.component.jsx
+++ opinion-app/src/components/input/text-box/text-box.component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./text-box.styles.css";
 
-export const TextBox = ({ placeholder, handleInputChange }) => {
+export const TextBox = ({ placeholder, handleInputChange, type }) => {
   return (
     <input
-      type="search"
+      type={type}
       className="textbox-field text-body"
       onChange={handleInputChange}
       placeholder={placeholder}
