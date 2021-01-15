f98b3e9f73af76344595dcbc9ea306e135fe4e72
Dongen,Cyrion C.M.N. van <cyrion.vandongen@student.fontys.nl>
2021-01-15 16:09:18 +0100

    Check for valid data createDiscussion

--- opinion-app/src/components/input/button/button-attention/button-attention.component.jsx
+++ opinion-app/src/components/input/button/button-attention/button-attention.component.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./button-attention.styles.css";
 import "../button.styles.css";
 
-export const ButtonAttention = ({ text, handleOnClick }) => {
+export const ButtonAttention = ({ text, handleOnClick, disabled=false }) => {
   return (
-    <button className="button-attention text-button" onClick={handleOnClick}>
+    <button className="button-attention text-button" onClick={handleOnClick} disabled={disabled}>
       {text}
     </button>
   );
